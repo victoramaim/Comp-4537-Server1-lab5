07fc09dc05e0e98ab467c0f241441fc246dbc2fa
victoramaim <vcarvalhodiasamaimva@my.bcit.ca>
2024-03-10 22:23:53 -0700

    Changed varibles from var to const

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('addButton').addEventListener('click', function () {
-        var query = document.getElementById('addData').value;
+        const query = document.getElementById('addData').value;
         sendData('POST', 'https://urchin-app-lradi.ondigitalocean.app/add', {query: query});
     });
 
     document.getElementById('retrieveButton').addEventListener('click', function () {
-        var query = document.getElementById('retrieveData').value;
+        const query = document.getElementById('retrieveData').value;
         sendData('GET', 'https://urchin-app-lradi.ondigitalocean.app/retrieve?query=' + encodeURIComponent(query));
     });
 
     function sendData(method, url, data) {
-        var xhr = new XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
         xhr.open(method, url, true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.onreadystatechange = function () {
